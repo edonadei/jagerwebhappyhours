28d5d9f0f3d605ba5a37f96513ccc1fe14124d7e
EDONADEI <emrick.donadei@hotmail.fr>
2018-03-26 23:30:12 +0200

    Ajout de l'edit + id / Fix des adresses css JS / Problème persistant sur API GOOGLE
    Ajout de l'edit + id / Fix des adresses css JS / Problème persistant sur API GOOGLE
    Manque encore l'ajout d'un serveur mongoDB externe

--- routes/main.js
+++ routes/main.js
@@ -15,11 +15,43 @@ router.get('/new', (req,res) => {
     res.render('Events/edit.html', {events: events});
 });
 
+router.get('/edit/:id', (req,res) => {
+    Event.findById(req.params.id).then(events => {
+        res.render('Events/edit.html', {events:events});
+    })
+
+})
+
 router.get('/:id', (req, res) => {
     Event.findById(req.params.id).then(events => {
             res.render('Events/show.html', {events: events});
         },
         err => res.status(500).send(err));
+});
+
+
+router.post('id?', (req,res) => {
+    new Promise((resolve,reject) => {
+        if (req.params.id) {
+            Event.findById(req.params.id).then(resolve.reject);
+        }
+        else {
+            resolve (new Event())
+        }
+    }).then(events => {
+        events.name = req.body.name;
+        events.hour = req.body.hour;
+        events.date = req.body.number;
+        events.description = req.body.description;
+        if (req.file) events.picture = req.file.filename;
+        events.promonumber = req.file.promonumber;
+        events.coordinates = req.file.coordinates;
+
+        return pokemon.save();
+
+    }).then(() => {
+        res.redirect('/');
+    },err => console.log(err));
 });
 
 module.exports = router;
